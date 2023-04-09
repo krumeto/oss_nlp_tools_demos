c6236750a1e5d39ab40552e5a715218c923ee34d
krumetoft <krum.arnaudov@ft.com>
2023-04-09 12:23:32 +0000

    rename package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # After changes, don't forget to run `python setup.py develop`
 
 setup(
-    name='oss_nlp_tools',
+    name='opennlptools',
     version='0.1.0',
     packages=find_packages()
 )
